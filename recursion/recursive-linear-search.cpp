55b251e40af3644b2fe78f96573fa0239402757a
SHREYANCE1 <shreyancejain94@gmail.com>
2019-06-12 03:20:35 +0530

    recursive-linear-search

---
+++ recursion/recursive-linear-search.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+// Time Complexity: The below algorithm runs in O(N), 
+// where N is the number of elements present in the array
+
+// Space Complexity: There is no extra space used however
+// the internal stack takes O(N) extra space for recursive calls
+
+int search(int arr[],int l,  int r, int key) {
+
+    if(r < 0) return -1;
+
+    if (arr[l] == key) {
+        return l;
+    } else if (arr[r] == key){ 
+        return r;
+    } else {
+        return search(arr, l+1, r-1, key);
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int key = 3;
+    int result = search(arr, 0, n, key);
+    cout << "result: found at: " << result << endl;
+}
