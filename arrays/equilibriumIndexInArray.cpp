2e661f66cd2aa945fdcc51e9eef25caae12b2325
SHREYANCE1 <shreyancejain94@gmail.com>
2019-06-26 08:45:04 +0530

    equilibrim_index_of_an_array

--- arrays/equilibriumIndexInArray.cpp
+++ arrays/equilibriumIndexInArray.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+/**
+ * Description - Equilibrium index of an array is an index such that
+ * the sum of elements at lower indexes is equal to 
+ * the sum of elements at higher indexes
+ */
+
 void printArray(int arr[], int n){
     for(int i = 0; i < n; i++) {
         cout  << arr[i] << " ";
@@ -9,7 +15,21 @@ void printArray(int arr[], int n){
     cout << endl;
 }
 
+int getEqmIndex(int arr[], int n){
+    int sum = 0, left_sum = 0;
+    for(int i = 0; i < n; i++)
+        sum += arr[i];
+    for(int i = 0; i < n; i++){
+        sum -= arr[i];
+        if (left_sum == sum) return i;
+        left_sum += arr[i];
+    }
+    return -1;
+}
+
 int main(){
-    int arr[] = {};
+    int arr[] = {-7, 1, 5, 2, -4, 3, 0};
     int n = sizeof(arr)/sizeof(arr[0]);
+    int equilibriumIndex = getEqmIndex(arr, n);
+    cout << "equilibrium Index " << equilibriumIndex << endl;
 }
