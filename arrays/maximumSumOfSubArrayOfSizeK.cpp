daa6aedbaee78b3a98e8d650d9d6eb043fd645e5
SHREYANCE1 <shreyancejain94@gmail.com>
2019-07-10 22:49:26 +0530

    maximum sum of sub array of size k

--- arrays/maximumSumOfSubArrayOfSizeK.cpp
+++ arrays/maximumSumOfSubArrayOfSizeK.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+/*If brute force require k steps
+ to process a window of size k
+and there were n windows in the sequence,
+then it would require n.k steps for burte force to complete
+but because only two elememts change with every move,
+we can achieve an overall running time proportional to 2n*/
+
 void printArray(int arr[], int n){
     int i = 0;
     while(i < n){
@@ -10,14 +17,20 @@ void printArray(int arr[], int n){
     }
     cout << endl;
 }
-
+// O(n) solution for finding maximum sum of 
+// a subarray of size k
 int maxSubArray(int arr[], int k, int n){
+    // k must be greater than n
     if (k > n) return -1;
     int sumOfWindow = 0, maxSum;
+    // compute sum of first window of size k
     for(int i = 0; i < k; i++){
         sumOfWindow += arr[i];
     }
     maxSum = sumOfWindow;
+    /* Compute sum of remaining windows by 
+    removing first element of the previous window 
+    and adding last element of the current window */
     for(int i = k; i < n; i++){
         sumOfWindow += arr[i] - arr[i-k];
         maxSum = max(sumOfWindow, maxSum);
