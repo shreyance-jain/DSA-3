96e9603efb70766d077d74c5ff1191c5150a3c2e
shreyance-jain <shreyancejain94@gmail.com>
2019-08-21 22:50:02 +0530

    square root of ineteger

--- searching/squareRoot.cpp
+++ searching/squareRoot.cpp
@@ -1,3 +1,34 @@
+/*
+Square root
+
+Problem Statement
+
+Given an integer x. The task is to find the square root of x. If x is not a perfect square, then return floor(√x).
+
+Input Format:
+First line of input contains number of testcases T. For each testcase, the only line contains the number x.
+
+Output Format:
+For each testcase, print square root of given integer.
+
+Constraints:
+1 ≤ T ≤ 1000
+1 ≤ x ≤ 107
+
+Example:
+Input:
+2
+5
+4
+
+Output:
+2
+2
+
+Explanation:
+Testcase 1: Since, 5 is not perfect square, so floor of square_root of 5 is 2.
+Testcase 2: Since, 4 is a perfect square, so its square root is 2.
+*/
 
 #include<bits/stdc++.h>
 using namespace std;
