408403ec716b5f12800a4c15e3c7a9f93eb342cc
shreyance-jain <shreyancejain94@gmail.com>
2019-08-27 22:39:44 +0530

    bubble sort

---
+++ sorting/bubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)
+       for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j+1])
+                swap(arr[j], arr[j+1]);
+}
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        cout<< arr[i] << " ";
+    cout << endl;
+}
+// Driver program to test above functions
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i = 0; i < n; i++)
+            cin >> arr[i];
+        bubbleSort(arr, n);
+        printArray(arr, n);
+    }
+}
