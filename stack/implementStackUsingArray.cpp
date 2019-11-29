c66b876742f5354ae13a511e4bf8daded554fd34
shreyance-jain <shreyancejain94@gmail.com>
2019-11-29 23:53:04 +0530

    implement stack using array

--- stack/implementStackUsingArray.cpp
+++ stack/implementStackUsingArray.cpp
@@ -1,3 +1,49 @@
+/* 
+Implement stack using array
+
+Problem Statement
+
+Implement a Stack using Array.
+
+Input Format:
+The first line of the input contains an integer 'T' denoting the number of test cases.
+Then T test cases follow.
+First line of each test case contains an integer Q denoting the number of queries . 
+A Query Q is of 2 Types:
+(i) 1 x   (a query of this type means  pushing 'x' into the stack)
+(ii) 2     (a query of this type means to pop element from stack and print the poped element)
+The second line of each test case contains Q queries seperated by space.
+
+Output Format:
+The output for each test case will  be space separated integers having
+-1 if the stack is empty else the element poped out from the stack .
+
+Constraints:
+1 <= T <= 100
+1 <= Q <= 100
+1 <= x <= 100
+
+Example:
+Input:
+1
+5
+1 2 1 3 2 1 4 2
+4
+2 1 4 1 5 2  
+
+Output:
+3 4
+-1 5
+
+Explanation:
+In the first test case for query 
+1 2    the stack will be {2}
+1 3    the stack will be {2 3}
+2       poped element will be 3 the stack will be {2}
+1 4    the stack will be {2 4}
+2       poped element will be 4
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -35,14 +81,17 @@ int main() {
     }
 }
 
-/* Function push to insert element into the stack */
+/* Function push to insert element into the stack 
+For push we simply increment top and assign arr[top] = element*/
 void MyStack :: push(int x) {
     // element will be inserted just above the previous element
     top++;
     arr[top] = x;
 }
 
-/* Function pop() to delete element from stack */
+/* Function pop() to delete element from stack
+For pop simply return top element and decrement top. If top is -1
+then no pop can happen */
 int MyStack :: pop() {
     // if top is -1, then there is no element in the stack
     if (top == -1)
@@ -53,3 +102,5 @@ int MyStack :: pop() {
     top--;
     return x;
 }
+
+// Company Tags: FaceSet, Goldman Sachs, Kritikal Solutions, Qualcomm, Visa
