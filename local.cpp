c386028ba42716002a68ca01946392096eee8032
shreyance-jain <shreyancejain94@gmail.com>
2019-12-16 00:11:42 +0530

    local.cpp

--- local.cpp
+++ local.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
+struct Node
+{
 	int data;
-	struct Node* left;
-	struct Node* right;
-	Node(int x) {
+	struct Node *left;
+	struct Node *right;
+	Node(int x)
+	{
 		data = x;
 		left = right = NULL;
 	}
@@ -13,27 +15,32 @@ struct Node {
 
 bool isIdentical(Node *, Node *);
 
-int main() {
+int main()
+{
 	int t;
 	cin >> t;
-	while(t--) {
+	while (t--)
+	{
 		// input first tree
-		map<int, Node*> m;
+		map<int, Node *> m;
 		int n;
 		cin >> n;
 		struct Node *root = NULL;
 		struct Node *child;
-		while(n--) {
+		while (n--)
+		{
 			Node *parent;
 			int n1, n2;
 			char lr;
 			cin >> n1 >> n2 >> lr;
-			if (m.find(n1) == m.end()) {
+			if (m.find(n1) == m.end())
+			{
 				parent = new Node(n1);
 				m[n1] = parent;
 				if (root == NULL)
 					root = parent;
-			} else
+			}
+			else
 				parent = m[n1];
 			child = new Node(n2);
 			m[n2] = child;
@@ -43,21 +50,24 @@ int main() {
 				parent->right = child;
 		}
 		// input second tree
-		map<int, Node*> mm;
+		map<int, Node *> mm;
 		int nn;
 		cin >> nn;
 		struct Node *rott = NULL;
-		while(nn--) {
+		while (nn--)
+		{
 			int n1, n2;
 			char lr;
 			Node *parent;
 			cin >> n1 >> n2 >> lr;
-			if (mm.find(n1) == mm.end()) {
+			if (mm.find(n1) == mm.end())
+			{
 				parent = new Node(n1);
 				mm[n1] = parent;
 				if (rott == NULL)
 					rott = parent;
-			} else
+			}
+			else
 				parent = mm[n1];
 			child = new Node(n2);
 			mm[n2] = child;
@@ -71,7 +81,6 @@ int main() {
 	return 0;
 }
 
-
 /*
 Approach:
 1. If both tree are empty then return 1
@@ -82,15 +91,16 @@ Approach:
 	(d) If a, b and c are true then return 1
 2. Else return 0 (one is empty and other is not)
 */
-bool isIdentical(Node *r1, Node *r2) {
+bool isIdentical(Node *r1, Node *r2)
+{
 	// both empty
 	if (r1 == NULL && r2 == NULL)
 		return true;
 	// both: non-empty, so compare them
-	if (r1 != NULL && r2 != NULL) {
+	if (r1 != NULL && r2 != NULL)
+	{
 		return (r1->data == r2->data &&
-			isIdentical(r1->left, r2->left)
-			&& isIdentical(r1->right, r2->right));
+				isIdentical(r1->left, r2->left) && isIdentical(r1->right, r2->right));
 	}
 	// if one empty and one not empty - false
 	return false;
@@ -101,20 +111,3 @@ bool isIdentical(Node *r1, Node *r2) {
 // O(m) where (m < n)
 
 // Company Tags: Amazon, Microsft
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
