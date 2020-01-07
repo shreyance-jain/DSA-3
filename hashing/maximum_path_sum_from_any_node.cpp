179084e650edb3d73a3759f9cb2e18a52eee8fbc
shreyance-jain <shreyancejain94@gmail.com>
2020-01-07 14:45:56 +0530

    max path sum from any node

---
+++ hashing/maximum_path_sum_from_any_node.cpp
@@ -0,0 +1,161 @@
+/* 
+Maximum path sum from any node
+
+Difficulty: Medium
+Problem Statement
+
+Given a binary tree of N edges, where every node contains distinct data.
+The task is to find the maximum path sum. The path may start and end at any node in the tree.
+
+Input:
+First line of input contains number of testcases T.
+For each testcase, there will be two lines,
+first of which containing the number of edges (between two nodes) in the tree.
+Next line contains N pairs (considering a and b) with a
+'L' (means node b on left of a) or 'R' (means node b on right of a) after a and b.
+
+Output:
+Print the maximum path sum in the binary tree.
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 103
+
+Example:
+Input:
+2
+6
+10 2 L 2 20 L 2 1 R 10 -25 R -25 3 L -25 4 R
+3
+10 2 L 10 5 R 5 -2 R
+
+Output:
+32
+17
+
+Explanation:
+Testcase 2: Path in the given tree goes like 2, 10, 5, which gives the max sum as 17.
+ */
+
+
+// C/C++ program to find maximum path sum in Binary Tree
+#include<bits/stdc++.h>
+using namespace std;
+
+// A binary tree node
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+int findMaxSum(Node*);
+
+void insert(struct Node *root,int n1,int n2,char lr)
+ {
+     if(root==NULL)
+        return;
+     if(root->data==n1)
+     {
+         switch(lr)
+         {
+          case 'L': root->left=new Node(n2);
+                    break;
+          case 'R': root->right=new Node(n2);
+                    break;
+         }
+     }
+     else
+     {
+         insert(root->left,n1,n2,lr);
+         insert(root->right,n1,n2,lr);
+     }
+}
+ 
+int main()
+{
+    /* Let us construct the tree shown in above diagram */
+    int t,k;
+    cin>>t;
+    while(t--)
+    {
+    int n;
+    cin>>n;
+    struct Node *root=NULL;
+    while(n--)
+    {
+        char lr;
+        int n1,n2;
+        cin>>n1>>n2;
+        cin>>lr;
+        if(root==NULL)
+        {
+            root=new Node(n1);
+            switch(lr){
+                    case 'L': root->left=new Node(n2);
+                    break;
+                    case 'R': root->right=new Node(n2);
+                    break;
+                    }
+        }
+        else
+        {
+            insert(root,n1,n2,lr);
+        }
+    }
+        cout<<findMaxSum(root)<<endl;
+        
+    }
+    return 0;
+}
+
+/* For each node there can be four ways that the max path goes through the node:
+1. Node only
+2. Max path throught Left Child + Node
+3. Max path through Right Child + Node
+4. Max path through Left Child + Max path trough right child
+
+The idea is to keep track of four paths and pick the max one in end. An important thing to note is,
+root of every subtree need to return maximum path sum such that at most one child of root is involved.
+This is needed for parent function call*/
+int getMaxSum(Node *root, int *maxSum) {
+    // base case, taking reference from maxSum code
+    if (root == NULL)
+        return 0;
+    
+    // recur for left
+    int left_sum = getMaxSum(root->left, maxSum);
+    // recur for right
+    int right_sum = getMaxSum(root->right, maxSum);
+    
+    // now max sum can be left_sum or right_sum (we are getting below root sum included in this sum
+    // that is why we are doing one extra comparison in main function)
+    // also check the possibility : left_sum + right_sum + root->data forms maxSum
+    // because in recurrence we are returing by taking on one child in consideration
+    *maxSum = max(max(max(left_sum, right_sum), left_sum + right_sum + root->data), *maxSum);
+    
+    // imp return sum such that at most one child is involved so we get correct values
+    // when we recur for left_sum and right_sum in code below
+    return max(left_sum, right_sum) + root->data;
+} // this function can also be written as in set-2
+
+int findMaxSum(Node* root)
+{ 
+    if (root == NULL)
+        return 0;
+    
+    int maxSum = 0;
+    int sum = getMaxSum(root, &maxSum);
+    // one extra comparison for including root sum
+    maxSum = max(sum, maxSum);
+    return maxSum;
+}
+
+// Time Complexity: O(n), simply traverse tree in preorder fashion and does constant work at every node
+// Company Tags: Accolite
--- local.cpp
+++ local.cpp
@@ -1,96 +1,69 @@
-/*
-Print a Binary Tree in Vertical Order | Set 1
-
-Given a binary tree, print it vertically. The following example illustrates vertical order traversal.
-
-           1
-        /    \
-       2      3
-      / \    / \
-     4   5  6   7
-             \   \
-              8   9 
-               
-              
-The output of print this tree vertically will be:
-4
-2
-1 5 6
-3 8
-7
-9 
- */
-
-/* We have discussed a O(n^2) soultion in set-1
-In this, we implement an efficient solution based on hashmap
-We need to check horizontal distances from root for all nodes.
-If two nodes have same horizontal distances(HD), then they are on same vertical lines.
-The idea of HD is simple. HD for root is 0, a right edge connecting to right subtree
-is considered as +1 horizontal distance and a left edge is considered as -1 horizontal distance.
-For example, in above tree, HD for node 4 is -2, HD for node 2 is -1, HD for 5 and 6 is 0 and HD for node 7 is +2
-We can do preorder travresal of the given binary tree. While traversing the tree, we can recusively calculate HDs
-We initially pass the horizontal distance for root as 0.
-For left subtree we pass the hd as hd of root -1 
-for right subtree we pass the hd as hd of root +1
-For every HD value, we maintain a list of nodes in a hash map.
-Whenever we see a node in traversal, we go to hash map entry and
-add the node to the hash map using HD as a key in map*/
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 
 struct Node {
 	int data;
 	struct Node *left;
 	struct Node *right;
-	Node (int x) {
+	Node(int x) {
 		data = x;
 		left = right = NULL;
 	}
 };
 
-/*fucntion to store vertical order in map 'm'
-'hd' is horizontal distance of current node from root
-'hd' is initially passed as 0*/
-void getVerticalOrder(Node *node, int hd, map<int, vector<int>> &m) {
-	// base case
-	if (node == NULL)
+Node *buildTreeWrap(int in[], int pre[], int n);
+Node *buildTree(int in[], int pre[], int inStrt, int inEnd, int *preIndex, unordered_map<int, int> &m);
+
+void printPostOrder(Node *root) {
+	if (root == NULL)
 		return;
-	// store current node in map 'm'
-	m[hd].push_back(node->data);
-	// store nodes in left subtree
-	getVerticalOrder(node->left, hd - 1, m);
-	// store nodes in right subtree
-	getVerticalOrder(node->right, hd + 1, m);
+	printPostOrder(root->left);
+	printPostOrder(root->right);
+	cout << root->data << " ";
 }
 
-
-void verticalOrder(Node *root) {
-	// map to store vertical order
-	map<int, vector<int>> m;
-	int hd = 0;
-	getVerticalOrder(root, hd, m);
-	map<int, vector<int>>::iterator it;
-	// traverse map and store nodes at each horizontal distance
-	for(it = m.begin(); it != m.end(); it++) {
-		for(int i = 0; i < it->second.size(); ++i)
-			cout << it->second[i] << " ";
+int main() {
+	int T;
+	cin >> T;
+	while(T--) {
+		int n, i;
+		cin >> n;
+		int in[n], pre[n];
+		for(i = 0; i < n; i++)
+			cin >> in[i];
+		for(i = 0; i < n; i++)
+			cin >> pre[i];
+		Node *root = buildTreeWrap(in, pre, n);
+		printPostOrder(root);
 		cout << endl;
 	}
 }
 
-void passByRef(int arr[]) {
-	arr[2] = -1;
+Node *buildTreeWrap(int in[], int pre[], int n) {
+	Node *root = NULL;
+	unordered_map<int, int> m;
+	for(int i = 0; i < n; i++)
+		m[in[i]] = i;
+	int preIndex = 0;
+	root = buildTree(in, pre, 0, n-1, &preIndex, m);
+	return root;
 }
 
-int main() {
-	int t[3] = {1, 0, 1};
-	passByRef(t);
-	cout << t[2] << endl;
-}
+Node *buildTree(int in[], int pre[], int inStrt, int inEnd, int *preIndex, unordered_map<int, int> &m) {
+	if (inStrt > inEnd)
+		return NULL;
 
-// Time Complexity: Time complexity of this hashing based solution cane be considered as O(n) under
-// the assumption that we have a good hashing function that allows insertion and retrieval operations
-// as O(1) time. In above C++ implementation, map of STL is used.
-// map in STL is typically impelemented using a Self-Balancing Binary Search Tree, where all operations
-// take O(logn) time. Therefore the time complexity of above solution is O(nlogn)
-// This solution MAY print nodes 	
+	Node *root = new Node(pre[*preIndex]);
+	(*preIndex)++;
+	
+	if (inStrt == inEnd)
+		return root;
+	
+	int inIndex = m[root->data];
+	
+	root->left = buildTree(in, pre, inStrt, inIndex - 1, preIndex, m);
+	root->right = buildTree(in, pre, inIndex + 1, inEnd, preIndex, m);
+
+	return root;
+}
