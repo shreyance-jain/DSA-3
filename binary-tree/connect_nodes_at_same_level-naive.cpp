f0372408d23518f03824936aa574ddbc5a78bc8c
shreyance-jain <shreyancejain94@gmail.com>
2020-03-14 14:18:04 +0530

    connect nodes at same level naive practice

---
+++ binary-tree/connect_nodes_at_same_level-naive.cpp
@@ -0,0 +1,270 @@
+/*
+Connect Nodes at Same Level
+
+Difficulty: Medium
+
+Problem Statement
+
+Given a binary tree, connect the nodes that are at same level.
+Structure of the given Binary Tree node is like following.
+struct Node
+{
+      int data;
+      Node* left;
+      Node* right;
+      Node* nextRight;
+}
+
+Initially, all the nextRight pointers point to garbage values. 
+Your function should set these pointers to point next right for each node.
+       10                       10 ------> NULL
+      / \                       /      \
+     3   5       =>     3 ------> 5 --------> NULL
+    / \     \               /  \           \
+   4   1   2          4 --> 1 -----> 2 -------> NULL
+
+Input Format:
+First line of input contains the number of test cases T.
+For each test case, there will be only a single line of input which is a string representing the tree as described below: 
+The values in the string are in the order of level order traversal of the tree where, 
+numbers denotes node values, and a character “N” denotes NULL child.
+For example:
+For the above tree, the string will be: 1 2 3 N N 4 6 N 5 N N 7 N
+
+Output Format:
+For each testcase, in a new line, print the level order and inorder traversals.
+
+Constraints:
+1 <= T <= 100
+1 <= Number of nodes <= 100
+1 <= Data of a node <= 1000
+
+Example:
+Input:
+2
+3 1 2
+10 20 30 40 60
+Output:
+3 1 2
+1 3 2
+10 20 30 40 60
+40 20 60 10 30
+
+Explanation:
+Testcase1: The connected tree is
+        3 ------> NULL
+     /      \
+   1 -----> 2 ------ NULL
+Testcase2: The connected tree is
+                           10 ----------> NULL
+                        /        \
+                     20 ------> 30 -------> NULL
+                  /       \
+               40 ----> 60 ----------> NULL
+ */
+
+ // { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node* nextRight;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->nextRight = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+void connect(struct Node *p);
+
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+
+
+void printSpecial(Node *root)
+{
+   if (root == NULL)
+     return;
+
+   Node* next_root=NULL;
+
+   while (root != NULL)
+   {
+      cout<< root->data<<" ";
+
+      if( root->left && (!next_root) )
+        next_root = root->left;
+      else if( root->right && (!next_root)  )
+        next_root = root->right;
+
+      root = root->nextRight;
+   }
+   
+   printSpecial(next_root);
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+       return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d\n", &t);
+  while (t--)
+  {
+     string s;
+     getline(cin, s);
+     Node* root = buildTree(s);
+
+     connect(root);
+     printSpecial(root);
+     cout<<endl;
+     inorder(root);
+     cout<<endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
+/* struct Node
+{
+  int data;
+  Node *left,  *right;
+  Node *nextRight;  // This has garbage value in input trees
+}; */
+
+/* 
+The approach here is to do simple level order traversal
+make connection for root (parent)
+make connection for child where connections for parent are already done
+so get next right using connections of parent
+the code has scope for improvement
+ */
+void connect(Node *p)
+{
+    if (p == NULL) return;
+    queue<Node *> q;
+    q.push(p);
+    p->nextRight = NULL;
+    while(q.empty() == false) {
+            Node *curr = q.front();
+            q.pop();
+            // if left child is present
+            if (curr->left != NULL) {
+                // if right child is also present, set nextright of left child as right child
+                if (curr->right != NULL) {
+                    curr->left->nextRight = curr->right;
+                } else { // get the next right using next right of parent and is applied for right child
+                    if (curr->nextRight == NULL) {
+                        curr->left->nextRight = NULL;
+                    } else {
+                        if (curr->nextRight->left != NULL) {
+                            curr->left->nextRight = curr->nextRight->left;
+                        } else if (curr->nextRight->right != NULL) {
+                            curr->left->nextRight = curr->nextRight->right;
+                        } else {
+                            curr->left->nextRight = NULL;
+                        }
+                    }
+                }
+                q.push(curr->left);
+            }
+            if (curr->right != NULL) {
+                if (curr->nextRight == NULL) {
+                        curr->right->nextRight = NULL;
+                    } else {
+                        if (curr->nextRight->left != NULL) {
+                            curr->right->nextRight = curr->nextRight->left;
+                        } else if (curr->nextRight->right != NULL) {
+                            curr->right->nextRight = curr->nextRight->right;
+                        } else {
+                            curr->right->nextRight = NULL;
+                        }
+                    }
+                q.push(curr->right);
+            }
+    }
+}
