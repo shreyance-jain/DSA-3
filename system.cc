e98a59f04450fc4b57f5ab55c4ad3a92f1bdc776
shreyance-jain <shreyancejain94@gmail.com>
2020-05-12 08:31:19 +0530

    system() in c/c++

---
+++ system.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/* 
+system() is used to invoke an operating system command from a C/C++ program.
+Using system(), we can execute any command that can run on terminal if operating system allows.
+For example, we can call system(“dir”) on Windows and system(“ls”) to list contents of a directory.
+ */
+
+int main() {
+	int arr[] = {1, 2, 3};
+	int k = 2;
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+	
+	// for(int i = 0; i <= k; i++)
+	// 	pq.push(arr[i]);
+
+	// make pair
+	// on pair min heap will work on key
+	// int c = 3;
+	// while(c--) {
+	// 	pq.push(1);
+	// }
+
+	// while(pq.empty() == false) {
+	// 	cout << pq.top() << " ";
+	// 	pq.pop();
+	// }
+	system("echo $name");
+
+	cout << endl;
+	return 0;
+}
