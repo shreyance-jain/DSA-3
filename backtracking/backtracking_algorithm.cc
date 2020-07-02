330e12991b513584a5defc3d7f6e779cf228e300
shreyance-jain <shreyancejain94@gmail.com>
2020-07-03 01:18:30 +0530

    BACKTRACKING ALGORITHM PARADIGM/BASIS

---
+++ backtracking/backtracking_algorithm.cc
@@ -0,0 +1,7 @@
+/* 
+BACKTRACKING ALGORITHM:
+Backtracking is an alogrithmic technique for solving problem recursively by trying to build a solution
+incrementally. Solving one piece at a time and remvoing those solutions which fail to satisfy the 
+constraints of a problem at any point of time(by time, here , is referred to the time elapsed
+till reaching any level of the search tree) it the process of backtracking
+ */
